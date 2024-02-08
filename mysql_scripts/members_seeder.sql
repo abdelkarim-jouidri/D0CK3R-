6abc37797b43ee42f209948c354c6e82cc9c8f70
abdelkrim-jouidri <abdelkrimjouidri@gmail.com>
2024-02-08 10:34:26 +0100

    removed unnecessary .git files

---
+++ mysql_scripts/members_seeder.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO member (firstname, lastname, joining_date, nationality, identity_document_type, identity_number)
+VALUES 
+  ('John', 'Doe', '2023-01-01', 'USA', '2', '123456789'),
+  ('Alice', 'Smith', '2023-02-01', 'Canada', '1', '987654321'),
+  ('Michael', 'Johnson', '2023-03-01', 'UK', '1', '456123789');
